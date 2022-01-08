0a1f9f7b1cafab3c68057d1a990c90f3630faa5a
KillyMXI <killy.mxi@gmail.com>
2022-01-08 17:02:13 +0300

    ensure file extensions in local import statements

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,28 @@
 import typescript from '@rollup/plugin-typescript';
 import cleanup from 'rollup-plugin-cleanup';
-import pkg from './package.json';
 
 export default [
   {
     external: [],
-    input: 'src/core.ts',
-    plugins: [typescript(), cleanup({ extensions: ['ts'] })],
-    output: [
-      { file: pkg.exports['.'].import, format: 'es' },
-      { file: pkg.exports['.'].require, format: 'cjs' },
-    ],
-  },
-  {
-    external: ['./core'],
     input: 'src/char.ts',
-    plugins: [typescript(), cleanup({ extensions: ['ts'] })],
+    treeshake: false,
+    plugins: [
+      typescript(),
+      cleanup({ extensions: ['ts'] })
+    ],
     output: [
-      { file: pkg.exports['./char'].import, format: 'es' },
-      { file: pkg.exports['./char'].require, format: 'cjs' },
+      {
+        dir: 'lib',
+        format: 'es',
+        preserveModules: true,
+        entryFileNames: '[name].mjs',
+      },
+      {
+        dir: 'lib',
+        format: 'cjs',
+        preserveModules: true,
+        entryFileNames: '[name].cjs',
+      },
     ],
   },
 ];
